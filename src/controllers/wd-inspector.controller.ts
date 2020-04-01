5a6289800048a65bd80734d21b8edf94dc34ab13
reluc <relu.cri@gmail.com>
2020-04-01 17:16:19 +0200

    feat: farm location

--- src/controllers/wd-inspector.controller.ts
+++ src/controllers/wd-inspector.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { get, post, param, requestBody, put, modelToJsonSchema } from '@loopback/rest';
+import { get, post, param, requestBody, put, modelToJsonSchema, del } from '@loopback/rest';
 import data from '../res/appezzamenti.out.json';
 import { model } from '@loopback/repository'
 
@@ -40,15 +40,27 @@ export class WdInspectorController {
     return JSON.stringify([
       {
         "name": "Bertacchini's Farm",
-        fields: data.filter(field => field.id === "2547")
+        location: {
+          lat: 44.780536,
+          long: 10.716421
+        },
+        fields: data.filter(field => field.id === "2547" || field.id === "27029")
       },
       {
         "name": "Ferrari's Farm",
+        location: {
+          lat: 44.790788,
+          long: 10.736074
+        },
         fields: data.filter(field => ["22602", "3097", "31310", "22603", "10735", "2079"].includes(field.id))
       },
       {
         "name": "Bonaccini's Farm",
-        fields: data.filter(field => ["23572", "23571"].includes(field.id))
+        location: {
+          lat: 44.774826,
+          long: 10.726659
+        },
+        fields: data.filter(field => ["23572", "23571", "12255", "13631"].includes(field.id))
       },
     ])
   }
@@ -108,6 +120,11 @@ export class WdInspectorController {
     return req;
   }
 
+  @del('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan/{request}')
+  deleteRequest(@param.path.string('field') field: string, @param.path.string('request') request: string) {
+    (plansData as any)[field] = (plansData as any)[field].filter((req: IrrigationRequest) => req.id !== request)
+  }
+
   @post('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan/{request}/status')
   setRequestStatus(@param.path.string('field') field: string,
     @param.path.string('request') request: string,
@@ -133,6 +150,7 @@ export class WdInspectorController {
 
 @model()
 export class IrrigationRequest {
+  id: string;
   waterVolume: Number;
   start: Date;
   status: IrrigationRequestStatus
--- src/controllers/wd-manager.controller.ts
+++ src/controllers/wd-manager.controller.ts
@@ -6,15 +6,27 @@ const model = {
   farms: [
     {
       "name": "Bertacchini's Farm",
-      fields: data.filter(field => field.id === "2547")
+      location: {
+        lat: 44.780536,
+        long: 10.716421
+      },
+      fields: data.filter(field => field.id === "2547" || field.id === "27029")
     },
     {
       "name": "Ferrari's Farm",
+      location: {
+        lat: 44.790788,
+        long: 10.736074
+      },
       fields: data.filter(field => ["22602", "3097", "31310", "22603", "10735", "2079"].includes(field.id))
     },
     {
       "name": "Bonaccini's Farm",
-      fields: data.filter(field => ["23572", "23571"].includes(field.id))
+      location: {
+        lat: 44.774826,
+        long: 10.726659
+      },
+      fields: data.filter(field => ["23572", "23571", "12255", "13631"].includes(field.id))
     },
   ],
   inspectors: ["1234"]

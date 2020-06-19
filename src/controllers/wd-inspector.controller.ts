75e709208b5e0b6fc8fbd82a1b6a9aa4ea4ffcd2
reluc <relu.cri@gmail.com>
2020-06-19 12:07:31 +0200

    refactor: remove unsued methods

--- src/controllers/wd-inspector.controller.ts
+++ src/controllers/wd-inspector.controller.ts
@@ -47,40 +47,14 @@ export class WdInspectorController {
     return JSON.stringify(await requestsByField(field))
   }
 
-  @post('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan')
-  create(@param.path.string('field') field: string, @requestBody({
-    required: true,
-    content: {
-      "application/json": {
-        schema: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              id: { type: "string" },
-              type: { type: "string", enum: ["criteria", "cbec"] },
-              channel: { type: "string" },
-              waterVolume: { type: "number" },
-              start: { type: "string", format: "date-time" },
-              status: { type: "string", enum: Object.keys(IrrigationRequestStatus) }
-            }
-          }
-        }
-      }
-    }
-  }) plan: IrrigationRequest[]): void {
-    (plansData as any)[field] = plan;
-  }
+
 
   @get('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan/{request}')
   async request(@param.path.string('field') field: string, @param.path.string('request') request: string) {
     return await irrigation(request);
   }
 
-  @del('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan/{request}')
-  deleteRequest(@param.path.string('field') field: string, @param.path.string('request') request: string) {
-    (plansData as any)[field] = (plansData as any)[field].filter((req: IrrigationRequest) => req.id !== request)
-  }
+
 
   @post('/v0/WDmanager/{id}/WDMInspector/{ispector}/AssignedFarms/{field}/irrigation_plan/{request}/status')
   async setRequestStatus(@param.path.string('field') field: string,
